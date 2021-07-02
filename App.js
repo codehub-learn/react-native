668c836503a05ba69e463c2773cb9e4ea17241e0
fakiolinho <m.fakiolas@hotmail.gr>
2021-07-03 02:19:56 +0300

    feat: add lab-1

--- App.js
+++ App.js
@@ -5,6 +5,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* Create a layout with a header a main area and a footer */}
+      {/* Header should have the word "Header" horizontally aligned */}
+      {/* Footer should have the word "Footer" horizontally aligned */}
+      {/* Header and Footer should have white text and black background color */}
+      {/* Main area should take the whole real estate vertically */}
+      {/* Put a text message in the Main area horizontally and vertically aligned */}
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
