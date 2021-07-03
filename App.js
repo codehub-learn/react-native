e9b399f80da2e62e110b25d363ab508b8efe0541
fakiolinho <m.fakiolas@hotmail.gr>
2021-07-03 03:37:53 +0300

    feat: add lab-5

--- App.js
+++ App.js
@@ -15,7 +15,10 @@ export default function App() {
         <Text style={styles.headerText}>Header</Text>
       </View>
       <View style={styles.main}>
-        <Text>Main</Text>
+        {/* Add a TextInput and a Button component in the center of main area */}
+        {/* The TextInput should be a controlled input */}
+        {/* First write whatever you like in the input element */}
+        {/* When clicking the button, trigger the Alert component by showing the input value */}
       </View>
       <View style={styles.footer}>
         <Text style={styles.footerText}>Footer</Text>
@@ -24,10 +27,19 @@ export default function App() {
   );
 }
 
-// Use a random platform color for iOS and android and set "#222" as the default background color
 const sectionStyle = {
   alignItems: "center",
-  backgroundColor: "#222",
+  ...Platform.select({
+    ios: {
+      backgroundColor: PlatformColor("systemTealColor"),
+    },
+    android: {
+      backgroundColor: PlatformColor("@android:color/holo_blue_bright"),
+    },
+    default: {
+      backgroundColor: "#222",
+    },
+  }),
   paddingVertical: 20,
 };
 
