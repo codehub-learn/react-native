e9b2fafed0cae89b5c91b45b3d73274a2a9fc812
fakiolinho <m.fakiolas@hotmail.gr>
2021-07-03 03:14:25 +0300

    feat: add lab-2

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
+    // Fix issue with status bar in iOS
     <View style={styles.container}>
-      {/* Create a layout with a header a main area and a footer */}
-      {/* Header should have the word "Header" horizontally aligned */}
-      {/* Footer should have the word "Footer" horizontally aligned */}
-      {/* Header and Footer should have white text and black background color */}
-      {/* Main area should take the whole real estate vertically */}
-      {/* Put a text message in the Main area horizontally and vertically aligned */}
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Header</Text>
+      </View>
+      <View style={styles.main}>
+        <Text>Main</Text>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Footer</Text>
+      </View>
     </View>
   );
 }
@@ -20,8 +21,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  main: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    alignItems: "center",
+    backgroundColor: "#222",
+    paddingVertical: 20,
+  },
+  headerText: {
+    color: "#fff",
+  },
+  footer: {
+    alignItems: "center",
+    backgroundColor: "#222",
+    paddingVertical: 20,
+  },
+  footerText: {
+    color: "#fff",
   },
 });
