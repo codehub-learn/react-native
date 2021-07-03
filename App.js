b256d32a53da86424ae74c6248620ce9f24b2697
fakiolinho <m.fakiolas@hotmail.gr>
2021-07-03 03:19:58 +0300

    feat: add lab-3

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    // Fix issue with status bar in iOS
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerText}>Header</Text>
       </View>
@@ -14,12 +13,14 @@ export default function App() {
       <View style={styles.footer}>
         <Text style={styles.footerText}>Footer</Text>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
+// Refactor duplicate styling
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#222",
     flex: 1,
   },
   main: {
